5590f9ab0d05fcaa9a29f73de76cd7d0d0c4cef0
KenwoodFox <kenwood364@gmail.com>
2022-09-08 20:51:11 -0400

    Clean more

--- godot/src/Graph.gd
+++ godot/src/Graph.gd
@@ -1,29 +1,28 @@
 extends Node2D
 
 # This array holds all the data for our graph
-var graphArray = [Vector2()]
+var graphPoints = PoolVector2Array([Vector2(0,0), Vector2(15,1), Vector2(30,4), Vector2(45, 17), Vector2(60, -1)])
 
 # Some constants
-var updatespeed = 2 # How many pixels we shift on every frame
+var updatespeed = 1 # How many pixels we shift on every frame
+var graphWidth = 300
 var graphColor = Color(255,0,0)
 
 
 func _ready():
-	# Remove these later, just populating some data
-	graphArray.append(Vector2(0, 0))
-	graphArray.append(Vector2(15, 1))
-	graphArray.append(Vector2(30, 4))
-	graphArray.append(Vector2(45, 17))
-	graphArray.append(Vector2(60, -1))
+	pass
 
 # Called whenever we want to redraw the graph (dont call manually)
 func _draw():
-	draw_polyline(graphArray, graphColor, 2)
+	draw_polyline(graphPoints, graphColor, 2)
 
 # Called regularly
 func _process(_delta):
-	for dp in graphArray:
-		dp += Vector2(-1,0)
+	for i in range(len(graphPoints)):
+		graphPoints[i] = graphPoints[i] + Vector2(-updatespeed,0)
+	
+	# Refine but, this adds data
+	graphPoints.append(Vector2((graphWidth / 2) - 50, Input.get_action_strength("ch_throttle") * -50))
 	
 	# Redraw every time (optimize later?)
 	update()
